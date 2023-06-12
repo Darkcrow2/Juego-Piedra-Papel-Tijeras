22a05508caa3c966b4df9ed4f807263e5ff7fe02
Charly Lopez <shadowlink1993@hotmail.com>
2023-06-11 23:06:43 -0600

    "Cambios en programacion"

--- PiedraPapelTijeras.py
+++ PiedraPapelTijeras.py
@@ -6,67 +6,62 @@ class JuegoPiedraPapelTijeras:
         self.puntosComputadora = 0
         self.ganador = False
         self.opciones = ["PIEDRA", "PAPEL", "TIJERAS", "SALIR"]
+        print("BIENVENIDO AL JUEGO DE PIEDRA, PAPEL O TIJERAS\n")
 
     def jugar(self):
 
-        print("BIENVENIDO AL JUEGO DE PIEDRA, PAPEL O TIJERAS\n")
+        opcionesUsuario = input(f"Elige entre {self.opciones[0]}, {self.opciones[1]}, {self.opciones[2]} y {self.opciones[3]}: ").upper()
+        opcionesComputadora = self.opciones[random.randint(0, 2)]
 
-        while True:
-            
-            opcionesUsuario = input(f"Elige entre {self.opciones[0]}, {self.opciones[1]}, {self.opciones[2]} y {self.opciones[3]}: ").upper()
-            opcionesComputadora = self.opciones[random.randint(0, 2)]
+        if (opcionesUsuario == self.opciones[3]):
+            print("\nJUEGO FINALIZADO")
+            print(f"EL PUNTAJE ES: \nUsuario: {self.puntosUsuario} \nComputadora: {self.puntosComputadora} \n")
+        elif (opcionesUsuario == self.opciones[0]):
+            match opcionesComputadora.lower():
+                case "piedra":
+                    print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nJuego EMPATADO\n")
+                case "papel":
+                    print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nComputadora GANA\n")
+                    self.puntosComputadora = self.puntosComputadora + 1
+                case "tijeras":
+                    print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nUsuario GANA\n")
+                    self.puntosUsuario = self.puntosUsuario + 1
 
-            if (opcionesUsuario == self.opciones[3]):
-                print("\nJUEGO FINALIZADO")
-                print(f"EL PUNTAJE ES: \nUsuario: {self.puntosUsuario} \nComputadora: {self.puntosComputadora} \n")
-                break;
-            elif (opcionesUsuario == self.opciones[0]):
-                match opcionesComputadora.lower():
-                    case "piedra":
-                        print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nJuego empatado\n")
-                    case "papel":
-                        print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nComputadora gana\n")
-                        self.puntosComputadora = self.puntosComputadora + 1
-                    case "tijeras":
-                        print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nUsuario gana\n")
-                        self.puntosUsuario = self.puntosUsuario + 1
+            self.ganador = self.puntaje(self.puntosUsuario, self.puntosComputadora)
+            if(self.ganador != True):
+                self.jugar()
+        elif (opcionesUsuario == self.opciones[1]):
+            match opcionesComputadora.lower():
+                case "piedra":
+                    print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nUsuario GANA\n")
+                    self.puntosUsuario = self.puntosUsuario + 1
+                case "papel":
+                    print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nJuego EMPATADO\n")
+                case "tijeras":
+                    print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nComputadora GANA\n")
+                    self.puntosComputadora = self.puntosComputadora + 1
 
-                self.ganador = self.puntaje(self.puntosUsuario, self.puntosComputadora)
-                if(self.ganador):
-                    break
+            self.ganador = self.puntaje(self.puntosUsuario, self.puntosComputadora)
+            if(self.ganador != True):
+                self.jugar()
+        elif (opcionesUsuario == self.opciones[2]):
+            match opcionesComputadora.lower():
+                case "piedra":
+                    print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nComputadora GANA\n")
+                    self.puntosComputadora = self.puntosComputadora + 1
+                case "papel":
+                    print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nUsuario GANA\n")
+                    self.puntosUsuario = self.puntosUsuario + 1
+                case "tijeras":
+                    print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nJuego EMPATADO\n")
 
-            elif (opcionesUsuario == self.opciones[1]):
-                match opcionesComputadora.lower():
-                    case "piedra":
-                        print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nUsuario gana\n")
-                        self.puntosUsuario = self.puntosUsuario + 1
-                    case "papel":
-                        print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nJuego empatado\n")
-                    case "tijeras":
-                        print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nComputadora gana\n")
-                        self.puntosComputadora = self.puntosComputadora + 1
+            self.ganador = self.puntaje(self.puntosUsuario, self.puntosComputadora)
+            if(self.ganador != True):
+                self.jugar()
 
-                self.ganador = self.puntaje(self.puntosUsuario, self.puntosComputadora)
-                if(self.ganador):
-                    break
-
-            elif (opcionesUsuario == self.opciones[2]):
-                match opcionesComputadora.lower():
-                    case "piedra":
-                        print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nComputadora gana\n")
-                        self.puntosComputadora = self.puntosComputadora + 1
-                    case "papel":
-                        print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nUsuario gana\n")
-                        self.puntosUsuario = self.puntosUsuario + 1
-                    case "tijeras":
-                        print(f"\nElegiste: {opcionesUsuario} \nLa computadora eligio: {opcionesComputadora} \nJuego empatado\n")
-
-                self.ganador = self.puntaje(self.puntosUsuario, self.puntosComputadora)
-                if(self.ganador):
-                    break
-
-            else:
-                print("Por favor, seleccione una opcion valida\n")
+        else:
+            print("Por favor, seleccione una opcion valida\n")
+            self.jugar()
 
     def puntaje(self, puntosUsuario, puntosComputadora):
         if(puntosUsuario == 5):
